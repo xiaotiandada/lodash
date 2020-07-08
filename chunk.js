897d893c50ce6d0ad351268893536f6664a11274
xiaotian <952822399@qq.com>
2020-07-08 18:43:17 +0800

    chunk

--- chunk.js
+++ chunk.js
@@ -1,53 +1,46 @@
-function chunk (array, size)  {
-  // 获取size, 如果小于0 取值为0
-  size = Math.max(size, 0)
-  // 获取数组长度 如果是null 为0, 否则为数组长度
-  const length = array === null ? 0 : array.length
-  // 如果没有长度或者切割小于0返回[]
+import { slice, toInteger } from 'lodash'
+
+/**
+ * Creates an array of elements split into groups the length of `size`.
+ * If `array` can't be split evenly, the final chunk will be the remaining
+ * elements.
+ *
+ * @since 3.0.0
+ * @category Array
+ * @param {Array} array The array to process.
+ * @param {number} [size=1] The length of each chunk
+ * @returns {Array} Returns the new array of chunks.
+ * @example
+ *
+ * chunk(['a', 'b', 'c', 'd'], 2)
+ * // => [['a', 'b'], ['c', 'd']]
+ *
+ * chunk(['a', 'b', 'c', 'd'], 3)
+ * // => [['a', 'b', 'c'], ['d']]
+ */
+
+
+function chunk (array, size = 1) {
+  // toInteger 返回转换后的整数
+  size = Math.max(toInteger(size), 0)
+  // 返回数组的长度
+  const length = array == null ? 0 : array.length
+
+  // 判断 length 和 size
   if (!length || size < 1) {
     return []
   }
-  // 开始 index
+  // 索引
   let index = 0
-  // 数组 index
   let resIndex = 0
-
-  // 数组数据 new Array 长度为 length/size 向上取整
+  // 结果数组
   const result = new Array(Math.ceil(length / size))
-
-  // 数组每个下标添加内容 开始index index+=size 结束index
-  while (index < length) {
-    // 这里的 _.slice 使用了 array.slice 代替
-    result[resIndex++] = array.slice(index, index+=size)
+  
+  while(index < length) {
+    // 代替 Array slice
+    result[resIndex++] = slice(array, index, ( index += size ))
   }
   return result
 }
 
-// const resChunk = chunk([1,2,3,4,5,6,7], 2)
-// console.log(resChunk);
-
-
-function chunkLite(array, size) {
-  // 获取长度
-  const length = array.length
-  // 如果没有长度或者切割小于0返回[]
-  if (!length || size < 1) return []
-  let index = 0 // 开始 index
-  let resIndex = 0 // 数组 index
-  let result = [] // 数组数据
-  // 循环 开始的索引小于数组长度执行
-  // 数组每个下标添加内容 开始index index+=size 结束index
-  while(index < length) result[resIndex++] = array.slice(index, index+=size)
-  return result
-}
-
-const resChunkLite = chunkLite([1,2,3,4,5,6,7], 1)
-console.log(resChunkLite);
-const resChunkLite1 = chunkLite([1,2,3,4,5,6,7], 2)
-console.log(resChunkLite1);
-const resChunkLite2 = chunkLite([1,2,3,4,5,6,7], 3)
-console.log(resChunkLite2);
-const resChunkLite3 = chunkLite([1,2,3,4,5,6,7], 4)
-console.log(resChunkLite3);
-const resChunkLite4 = chunkLite([1,2,3,4,5,6,7], 5)
-console.log(resChunkLite4);
+export default chunk
